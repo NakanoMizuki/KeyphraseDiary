9e773381a3f2da6ee93bb7b951accb4f969560f1
MizukiNakano <mizuki590@gmail.com>
2015-07-19 20:17:39 +0900

    Use Dialog for delete

--- app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/MemoDetail.java
+++ app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/MemoDetail.java
@@ -1,6 +1,8 @@
 package jp.ac.titech.psg.nakano.keyphrasememo.activities;
 
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,6 +18,7 @@ public class MemoDetail extends AppCompatActivity {
 
     private static final String TAG = "MemoDetail";
     private Memo memo;
+    private AlertDialog deleteDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,19 @@ public class MemoDetail extends AppCompatActivity {
 
         memo = (Memo) getIntent().getSerializableExtra("memo");
         Log.d(TAG, memo.toString());
+
+        // dialog for deleteButton
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delete_dialog_message);
+        builder.setPositiveButton(R.string.delete_dialog_positive, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MemoTableHelper tableHelper = new MemoTableHelper(MemoDetail.this);
+                tableHelper.deleteMemo(memo.getId());
+            }
+        });
+        builder.setNegativeButton(R.string.delete_dialog_negative, null);
+        deleteDialog = builder.create();
 
         // fragment
         MemoFragment fragment = new MemoFragment();
@@ -58,7 +74,6 @@ public class MemoDetail extends AppCompatActivity {
     }
 
     public void clickDelete(View v){
-        MemoTableHelper tableHelper = new MemoTableHelper(this);
-        tableHelper.deleteMemo(memo.getId());
+        deleteDialog.show();
     }
 }
