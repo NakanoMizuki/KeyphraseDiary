e47ffa6c4c08fb9c784bfac74f66ae3f814b598a
MizukiNakano <mizuki590@gmail.com>
2015-07-21 18:07:15 +0900

    New Activity

--- app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/SearchByTag.java
+++ app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/SearchByTag.java
@@ -1,31 +1,24 @@
 package jp.ac.titech.psg.nakano.keyphrasememo.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import jp.ac.titech.psg.nakano.keyphrasememo.R;
 
-
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-
+public class SearchByTag extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_search_by_tag);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_search_by_tag, menu);
         return true;
     }
 
@@ -43,20 +36,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void goWriteActivity(View v){
-        Intent intent = new Intent(this, WriteMemo.class);
-        startActivity(intent);
-    }
-
-    public void goReadActivity(View v){
-        Intent intent = new Intent(this, ReadMemo.class);
-        startActivity(intent);
-    }
-
-    public void goSearchActivity(View v){
-        Intent intent = new Intent(this, SearchByTag.class);
-        startActivity(intent);
-    }
-
 }
