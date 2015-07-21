8dab898394c2e8c7144b211bffa17b86ce3a60f8
MizukiNakano <mizuki590@gmail.com>
2015-07-21 11:12:09 +0900

    Refactoring

--- app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/fragments/ReadMemoFragment.java
+++ app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/fragments/ReadMemoFragment.java
@@ -6,10 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import jp.ac.titech.psg.nakano.keyphrasememo.R;
-import jp.ac.titech.psg.nakano.keyphrasememo.activities.AbstractWriteActivity;
 
 public class ReadMemoFragment extends Fragment {
 
