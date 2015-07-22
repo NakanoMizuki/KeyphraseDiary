44ab70a3fbbaf176bd0566de2fa5dd34c96fada1
MizukiNakano <mizuki590@gmail.com>
2015-07-22 11:55:36 +0900

    Refactoring: LinearLayout -> ViewGroup

--- app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/AbstractWriteActivity.java
+++ app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/AbstractWriteActivity.java
@@ -2,7 +2,6 @@ package jp.ac.titech.psg.nakano.keyphrasememo.activities;
 
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -56,8 +55,8 @@ abstract public class AbstractWriteActivity extends AppCompatActivity {
 
 
     public void createTagView(String name){
-        final LinearLayout container = (LinearLayout) this.findViewById(CONTAINER_ID);
-        final LinearLayout subContainer = new LinearLayout(this);
+        final ViewGroup container = (ViewGroup) this.findViewById(CONTAINER_ID);
+        final ViewGroup subContainer = new LinearLayout(this);
         EditText tagEdit = new EditText(this);
         tagEdit.setText(name);
         subContainer.addView(tagEdit, createParam());
@@ -66,8 +65,6 @@ abstract public class AbstractWriteActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("remove", "remove");
-                ViewGroup container = (ViewGroup) AbstractWriteActivity.this.findViewById(CONTAINER_ID);
                 container.removeView(subContainer);
                 container.invalidate();
             }
@@ -82,9 +79,9 @@ abstract public class AbstractWriteActivity extends AppCompatActivity {
 
     public List<String> getTagNames(){
         List<String> tags = new ArrayList<String>();
-        LinearLayout container = (LinearLayout) findViewById(CONTAINER_ID);
+        ViewGroup container = (ViewGroup) findViewById(CONTAINER_ID);
         for(int i=0; i < container.getChildCount(); ++i){
-            LinearLayout subContiner = (LinearLayout) container.getChildAt(i);
+            ViewGroup subContiner = (ViewGroup) container.getChildAt(i);
             EditText tagEdit = (EditText) subContiner.getChildAt(0);
             String tagName = tagEdit.getText().toString();
             if(!tags.contains(tagName) && !tagName.equals("")){
