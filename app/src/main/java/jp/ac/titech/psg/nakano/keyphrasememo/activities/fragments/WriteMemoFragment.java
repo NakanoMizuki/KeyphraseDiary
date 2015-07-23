0a4154332346b4ce536ad9a8c60f8e35d16e7140
MizukiNakano <mizuki590@gmail.com>
2015-07-23 16:34:19 +0900

    Add button

--- app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/fragments/WriteMemoFragment.java
+++ app/src/main/java/jp/ac/titech/psg/nakano/keyphrasememo/activities/fragments/WriteMemoFragment.java
@@ -4,16 +4,25 @@ package jp.ac.titech.psg.nakano.keyphrasememo.activities.fragments;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Hashtable;
+import java.util.Map;
 
 import jp.ac.titech.psg.nakano.keyphrasememo.R;
 import jp.ac.titech.psg.nakano.keyphrasememo.activities.AbstractWriteActivity;
 
 
-public class WriteMemoFragment extends android.support.v4.app.Fragment {
+public class WriteMemoFragment extends android.support.v4.app.Fragment implements View.OnClickListener{
+
+    private static final Map<Integer, String> mdMap = new Hashtable<Integer, String>(){{
+        put(R.id.fragment_write_memo_h1, "# ");
+    }};
 
     public static WriteMemoFragment newInstance() {
         WriteMemoFragment fragment = new WriteMemoFragment();
@@ -53,7 +62,7 @@ public class WriteMemoFragment extends android.support.v4.app.Fragment {
                 parent.setMemoTitle(s.toString());
             }
         });
-        EditText editContent = (EditText) parent.findViewById(R.id.fragment_write_memo_content);
+        final EditText editContent = (EditText) parent.findViewById(R.id.fragment_write_memo_content);
         editContent.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
@@ -64,6 +73,15 @@ public class WriteMemoFragment extends android.support.v4.app.Fragment {
                 parent.setMemoContent(s.toString());
             }
         });
+        Button h1 = (Button) parent.findViewById(R.id.fragment_write_memo_h1);
+        h1.setOnClickListener(this);
     }
+
+   @Override
+   public void onClick(View v) {
+       int id = v.getId();
+       String insert = mdMap.get(id);
+       Log.d("WriteMemo", "Please insert:" + insert);
+   }
 
 }
